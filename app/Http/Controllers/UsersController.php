8cd28c82eaaf2919dd104f805fb24f3874feadb3
xiwu962464 <85954875@qq.com>
2019-03-19 05:51:45 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,15 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|cinfirmed|min:6'
+        ]);
+
+        return;
+    }
 }
