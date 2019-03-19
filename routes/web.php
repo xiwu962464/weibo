8cd11b4ee3e288fdb8a7fadc6306c502cc91b985
xiwu962464 <85954875@qq.com>
2019-03-19 05:33:58 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,17 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+
+//遵从 RESTful 架构为用户资源生成路由。该方法接收两个参数，第一个参数为资源名称，第二个参数为控制器名称。
+Route::resource('users', 'UsersController');
+
+// 等同于
+/*
+    Route::get('/users', 'UsersController@index')->name('users.index');
+    Route::get('/users/create', 'UsersController@create')->name('users.create');
+    Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+    Route::post('/users', 'UsersController@store')->name('users.store');
+    Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+    Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+    Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+*/
