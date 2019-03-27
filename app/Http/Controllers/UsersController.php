c8c5c3fefdd46943c0643d69777b145e1bf72ecb
xiwu962464 <85954875@qq.com>
2019-03-27 03:11:31 +0000

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -106,13 +106,14 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'summer@example.com';
-        $name = 'Summer';
+        // $from = 'summer@example.com';
+        // $name = 'Summer';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
         Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+            // $message->from($from, $name)->to($to)->subject($subject);
+            $message->to($to)->subject($subject);
         });
     }
 
